cfcda79020c03ac7a93cf3948a32ba4ca9dc09f8
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2021-08-11 19:03:00 -0500

    Se agrega maven al proyecto

---
+++ src/main/java/co/edu/utadeo/Main.java
@@ -0,0 +1,22 @@
+package co.edu.utadeo;
+
+import co.edu.utadeo.controller.App;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new App();
+                frame.setSize(300,300);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
+
+}
